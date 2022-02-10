ffddff193ded9a734640a8bd4e661bda93d47af7
dlscoccia <dlscoccia@gmail.com>
2022-02-10 18:45:51 -0500

    task 3

--- 0x03-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.test.js
@@ -3,7 +3,7 @@ import React from "react";
 import App from "./App";
 
 describe("<App />", () => {
-  it("App renders without crashing", () => {
+  test("App renders without crashing", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.exists()).toEqual(true);
   });
